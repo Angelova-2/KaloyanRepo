37984e1f563ece5cf12822bdafe23e5f82db4027
Kaloyan Petrov <kpe3779@gmail.com>
2026-05-26 13:09:12 +0300

    Add files via upload

---
+++ Izpitwane/StackArray/ArrayStack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackArray
+{
+    public class ArrayStack<T>
+    {
+        private T[] elements;
+        public int Count { get; private set; }
+        private const int InitialCapacity = 16;
+        public ArrayStack(int capacity = InitialCapacity) 
+        {
+            this.elements = new T[capacity];
+            this.Count = capacity;
+
+
+        }
+        public void Push(T element) 
+        {
+            element = elements[this.Count];
+            this.Count++;
+
+   
+
+            if (this.Count == this.elements.Length)
+            {
+                Grow();
+            }
+        }
+        public T Pop() 
+        {
+            this.Count--;
+            return this.elements[this.Count];
+        }
+        public T[] ToArray() 
+        {
+            object[] array = new object[Count];
+            for (int i = 0; i < Count; i++)
+            {
+                array[i] = this.elements[i];
+
+            }
+            return this.elements;
+
+        }
+        private void Grow() 
+        {
+            object[] newElements = new object[2 * this.elements.Length];
+            for (int i = 0; i < this.Count; i++)
+            {
+                newElements[i] = this.elements;
+            }
+            
+        }
+
+    }
+}
---
+++ Izpitwane/StackArray/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StackArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayStack<int> name = new ArrayStack<int> ();
+
+            
+        }
+    }
+}
